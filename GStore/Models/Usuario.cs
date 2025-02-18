72f350517da35c3ca0ba0c216cf7f54ffd36b8bc
nickicn <nicolelopes0008@gmail.com>
2025-02-18 11:36:18 -0300

    Realizada a camada de dados

---
+++ GStore/Models/Usuario.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace GStore.Models;
+
+public class Usuario :IdentityUser
+{
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+    [StringLength(60, ErrorMessage = "O nome deve possuir no máximo 60 caracteres")]
+    public string Nome { get; set; }        
+
+    [Display(Name = "Data de Nascimento")]
+    [DataType(DataType.Date)]
+    public DateTime DataNascimento { get; set; }
+    
+    [StringLength(200)]
+    public string Foto { get; set; }
+}
